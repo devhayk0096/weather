6b9397d480a9d5166e32509e524280e3a3f1049c
hayk <devhayk0096@gmail.com>
2021-04-07 08:15:31 +0400

    created routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\SearchController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [PagesController::class, 'home'])->name('home');
+Route::get('results', [PagesController::class, 'results'])->name('results');
+
+Route::get('autocomplete-search', [SearchController::class, 'autoCompleteSearch'])->name('autocomplete.search');
+Route::get('searched-results', [SearchController::class, 'searchedResults'])->name('searched.results');
+Route::post('city-weather', [SearchController::class, 'getStoreCityWeather'])->name('city.weather');
